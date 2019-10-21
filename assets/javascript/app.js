2fb306bc3ede3aa7ad2822e6879e44ddc717ef46
Christopher Hayashi <Chrisrhayashi@gmail.com>
2019-10-21 14:12:03 -0700

    added html content and started pseudocoding app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,50 @@
+//Create a variable called questionArray holding all the questions
+
+//Create a variable called answerArray holding all the correct answers
+
+//Create a variable called questionIndex initialized to 0
+
+//Create a variable called timeRemaining initialized to 0
+
+//Create a variable called correctCounter initialized to 0
+
+//Create a variable called incorrectCounter initialized to 0
+
+//Create a variable called unansweredCounter initialized to 0
+
+//Create a function called countDown that displays the time remaining
+
+//set timeRemaining to 30
+
+//decrement timeRemaining by 1
+
+//display timeRemaining to index.html by using the id #timeRemaining
+
+//Create a function called displayQuestion
+
+//create a variable nextQuestion equal to the next question in questionArray
+
+//Display the question on the screen by using the id #questionDisplayed
+
+//Create a function called displayAnswers
+
+//Create a function called questionPage
+
+    //call countDown()
+
+    //call displayQuestion()
+
+    //call displayAnswers()
+
+//Create a function called losePage
+
+    //
+
+//Create a function called winPage
+
+//Create an onClick event for the start button
+
+    //delete the start button
+
+    //call questionPage()
+//
